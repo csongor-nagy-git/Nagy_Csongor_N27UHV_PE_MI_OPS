23bc94f43d2030294ad9344e29f18e7708f462ff
csongor-nagy-git <n.csongi97@gmail.com>
2024-11-23 00:21:42 +0100

    load_model.py first version

--- src/api/load_model.py
+++ src/api/load_model.py
@@ -1,13 +1,17 @@
 import mlflow
 import mlflow.keras
 import numpy as np
-from tensorflow.keras.preprocessing import image
-
+import mlflow.pyfunc
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000") 
 run_id = "a23e98f2355c419f9eaa3a380f2bea2b"
 
 model_uri = f"runs:/{run_id}/model"  # Modell elérési útvonal
-model = mlflow.keras.load_model(model_uri)
+model = mlflow.pyfunc.load_model(model_uri)
 
-model.summary()
+# Make predictions with the loaded model
+input_data = np.zeros((1, 224, 224, 3))  # Example input (10 dummy samples)
+predictions = model.predict(input_data)
+predicted_class = np.argmax(predictions, axis=1)
+print("Predictions:", predictions)
+print("Predicted class:", predicted_class)
