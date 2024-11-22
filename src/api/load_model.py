dca116acf2851f198c208d3c1e091a24636a53e7
csongor-nagy-git <n.csongi97@gmail.com>
2024-11-22 23:20:40 +0100

    Model uploads from TPU

--- src/api/load_model.py
+++ src/api/load_model.py
@@ -1,14 +1,13 @@
 import mlflow
-import pandas as pd
+import mlflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing import image
 
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000") 
-run_id = "05db52571628456583b07cf2d94ae00e"
+run_id = "a23e98f2355c419f9eaa3a380f2bea2b"
 
-model =  mlflow.sklearn.load_model(f"runs:/{run_id}//model")
+model_uri = f"runs:/{run_id}/model"  # Modell elérési útvonal
+model = mlflow.keras.load_model(model_uri)
 
-#%% működik-e?
-data = pd.read_csv("./data/cars.csv", sep=";")
-client = mlflow.tracking.MlflowClient(tracking_uri="http://127.0.0.1:5000")
-run_data_dict = client.get_run(run_id).data.to_dictionary()
-print(model.predict(data.loc[:, eval(run_data_dict["params"]["input"])]))
+model.summary()
--- src/api/model_uploader.py
+++ src/api/model_uploader.py
@@ -1,12 +1,15 @@
 import mlflow
 import mlflow.tensorflow
 import tensorflow as tf
+import numpy as np
 
 # Model betöltése .h5 formátumból
 model = tf.keras.models.load_model('C:\\Users\\Csocsesz\\Downloads\\model.h5')
 
 # Lokálisan futó MLFlow példány beállítása
 mlflow.set_tracking_uri("http://localhost:5000")
+
+
 
 # Modell feltöltése MLFlow-ba
 with mlflow.start_run():
