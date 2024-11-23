e261f464adf20ef50717936badeabec685de0374
csongor-nagy-git <n.csongi97@gmail.com>
2024-11-23 01:26:34 +0100

    Added Rabbit and pika to command.py and back_end.py

--- src/api/back_end.py
+++ src/api/back_end.py
@@ -1,19 +1,35 @@
-from fastapi import FastAPI
-import mlflow
-import mlflow.pyfunc
 from contextlib import asynccontextmanager
+from fastapi import FastAPI, Request
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import pika
+import time
+import logging
+
 model = None
 signature = None
 app = FastAPI()
 
-@asynccontextmanager # soon after imports
+@asynccontextmanager
 async def lifespan(app: FastAPI):
-    # at start
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    global client, rabbit_connection, channel
+    client = mlflow.tracking.MlflowClient(tracking_uri="http://127.0.0.1:5000")
+    credentials = pika.PlainCredentials(username="guest", password="guest")
+    while rabbit_connection is None:
+        try:
+            rabbit_connection = pika.BlockingConnection(pika.ConnectionParameters(host = "localhost", port = 5672, credentials=credentials, heartbeat=0))
+        except pika.exceptions.AMQPConnectionError:
+            logging.error(f"Connection to RabbitMQ failed at localhost:5672. Retrying...")
+            time.sleep(0.3)
+    channel = rabbit_connection.channel()
+    channel.basic_qos(prefetch_count=1)
+                
     yield
-    # at stop
+    channel.close()
+    rabbit_connection.close()
     return
-# after asnyccontextmanager
+
 app = FastAPI(lifespan=lifespan)
 
 @app.get("/") 
--- src/api/commands.py
+++ src/api/commands.py
@@ -1,4 +1,14 @@
 import requests
+import pandas as pd
+import pika
+
+def post_data(data, queue_name, host = "localhost", port = 5672, user = "guest", password = "guest"):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port, credentials=pika.PlainCredentials(user, password)))
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name, durable=True)
+    channel.basic_publish(exchange='', routing_key=queue_name, body=data.encode('utf-8'))
+    channel.close()
+    connection.close()
 
 if __name__ == "__main__":
     url = "http://localhost:8000"
